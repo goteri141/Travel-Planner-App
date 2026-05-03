41db9f40fe65f7c1f19de47d17b54d31b261bbde
Gloria Oteri <gloriaoteri@gmail.com>
2026-05-03 13:09:48 -0400

    Updated register method, so user will be assigned an ID

--- lib/services/authentication_service.dart
+++ lib/services/authentication_service.dart
@@ -1,12 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthenticationService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<UserCredential> register({required String name, required String email, required String password}) async{
-    return _auth.createUserWithEmailAndPassword(
+  // User will be assigned a user ID when they first register
+  Future<void> register({required String name, required String email, required String password}) async{
+    final userCredential = await _auth.createUserWithEmailAndPassword(
       email: email,
-      password: password);
+      password: password
+      );
+
+      final userID = userCredential.user!.uid;
+
+      await _firestore.collection('users').doc(userID).set({
+        'name': name,
+        'email': email
+      });
+    
   }
 
   Future<UserCredential> signIn({required String email, required String password}) async{
